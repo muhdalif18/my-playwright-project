aa3366ed413dcd4d7ac1f73e056f994332e9093c
Syed Alif <156448638+muhdalif18@users.noreply.github.com>
2025-10-28 16:43:54 +0800

    Add GitHub Actions for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     headless: true,
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("homepage has title", async ({ page }) => {
+  // Gunakan BASE_URL dari environment
+  if (!process.env.BASE_URL) {
+    throw new Error("BASE_URL is not defined");
+  }
+  await page.goto(process.env.BASE_URL);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Contoh: verify title page
+  await expect(
+    page.getByText("Welcome to AL AiN IT Consultants", { exact: true })
+  ).toBeVisible();
 });
